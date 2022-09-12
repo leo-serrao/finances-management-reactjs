20c3d740c8edb15432778af24e90ee4a969de134
Leonardo Serrão <leoserraom@gmail.com>
2022-09-11 22:11:32 -0300

    Adding useCallback to 'crateTransaction'

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
 import { api } from "../lib/axios";
 
 interface Transaction {
@@ -43,19 +49,22 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     setTransactions(response.data);
   }
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { category, description, price, type } = data;
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { category, description, price, type } = data;
 
-    const response = await api.post("transactions", {
-      description,
-      category,
-      price,
-      type,
-      createdAt: new Date(),
-    });
+      const response = await api.post("transactions", {
+        description,
+        category,
+        price,
+        type,
+        createdAt: new Date(),
+      });
 
-    setTransactions((state) => [response.data, ...state]);
-  }
+      setTransactions((state) => [response.data, ...state]);
+    },
+    []
+  );
 
   useEffect(() => {
     fetchTransactions();
